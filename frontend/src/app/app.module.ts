d84d4d6278358cf3cda88cc597f27183e01e8a68
Pavananon <killkinggamer@gmail.com>
2023-05-25 15:25:36 +0700

    board html

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
