18db02d7d59e16aea9a7beb7a99b5515daef028d
Pavananon <killkinggamer@gmail.com>
2023-05-26 12:02:37 +0700

    create board

--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-import { cards } from '../cards.service';
+import { Component, OnInit } from '@angular/core';
+import { cardInsert } from '../cards';
+import { CardsService } from "../cards.service"
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
+  ngOnInit(){
+    this.show()
+  }
+
+  done: cardInsert[] = []
+
+  constructor(
+      private cardService : CardsService
+  ) { }
+
+  show(){
+    return this.cardService.getPlaing()
+    .subscribe((e)=>{this.done=e})
+  }
 
 }
