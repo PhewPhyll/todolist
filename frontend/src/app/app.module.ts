20cd09f3b736a7bad43dd3d9b1797611821e0b99
Pavananon <killkinggamer@gmail.com>
2023-05-27 10:59:11 +0700

    update app and board

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoardComponent } from './board/board.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BoardComponent } from './board/board.component';
+
+// test
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent
+    BoardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    DragDropModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { cardInsert } from '../cards';
-import { CardsService } from "../cards.service"
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent implements OnInit {
-  ngOnInit(){
-    this.show()
-  }
-
-  done: cardInsert[] = []
-
-  constructor(
-      private cardService : CardsService
-  ) { }
-
-  show(){
-    return this.cardService.getPlaing()
-    .subscribe((e)=>{this.done=e})
-  }
+export class BoardComponent {
 
 }
